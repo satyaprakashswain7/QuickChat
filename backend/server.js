663a03a1c3abf37dc3879a38b57544e4da0ccb02
Programmer <Programmer@example.com>
2024-07-03 21:30:42 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const server = app.listen(PORT, console.log(`listening on ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
   },
 });
 
