765a8ce70241cb2035d8a4d034be3f719a491efb
Programmer <Programmer@example.com>
2024-07-03 21:49:40 +0530

    cors creds

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const path = require("path");
 const app = express();
 dotenv.config();
 connectDB();
-app.use(cors({ origin: "*" }));
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json()); //accepting json data from frontend
 
